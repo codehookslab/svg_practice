f48e9577238f563c72d393cd17df61303572a5ca
codehooks <codehookshelp@gmail.com>
2019-08-21 17:57:11 +0530

    Methods to add line, circle and texts are added.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import '../dist/style.css'
 
 import './util'
 import { add } from "./util";
+import { interval } from 'rxjs'
+import { pipe, take, map } from 'rxjs/operators'
 
 console.log('Application started')
 
 add.title('Simple Demo')
 add.footer('Copyrights @ Code-Hooks')
+add.line();
+
+interval(1000)
+    .pipe(
+        take(10),
+        map(v => v + 74)
+    ).subscribe(add.circle)
--- src/util.js
+++ src/util.js
@@ -1,4 +1,7 @@
+var xmlns = "http://www.w3.org/2000/svg";
+
 let titleEl = document.querySelector('#title')
+let svgContainer = document.querySelector('#svg-container')
 
 let title = (value) => {
     titleEl.innerHTML = value;
@@ -8,7 +11,51 @@ let footer = (value) => {
     document.querySelector('#footer').innerHTML = value;
 }
 
+let line = () => {
+    var svgLine = document.createElementNS(xmlns, 'line')
+    svgLine.setAttributeNS(null, 'x1', 50);
+    svgLine.setAttributeNS(null, 'y1', 50);
+    svgLine.setAttributeNS(null, 'x2', 750);
+    svgLine.setAttributeNS(null, 'y2', 50);
+    svgLine.setAttributeNS(null, 'stroke', 'red');
+    svgLine.setAttributeNS(null, 'stroke-width', 3);
+
+    svgContainer.appendChild(svgLine)
+}
+let index = 0;
+let circle = (value) => {
+    ++index
+    var svgCircle = document.createElementNS(xmlns, 'circle')
+    svgCircle.setAttributeNS(null, 'cx', (value + (50 * index)));
+    svgCircle.setAttributeNS(null, 'cy', 50);
+    svgCircle.setAttributeNS(null, 'r', 15);
+    svgCircle.setAttributeNS(null, 'stroke', 'black');
+    svgCircle.setAttributeNS(null, 'stroke-width', 2);
+    svgCircle.setAttributeNS(null, 'fill', 'white');
+
+    svgContainer.appendChild(svgCircle);
+    text({
+        x: (value + (49 * index)),
+        y: 55,
+        fill: 'red',
+        value: index
+    });
+}
+
+let text = ({ x, y, fill, value }) => {
+    var svgText = document.createElementNS(xmlns, 'text')
+    svgText.setAttributeNS(null, 'x', x)
+    svgText.setAttributeNS(null, 'y', y)
+    svgText.setAttributeNS(null, 'fill', fill)
+    svgText.innerHTML = value;
+    svgContainer.appendChild(svgText);
+}
+
 export const add = {
     title: title,
-    footer: footer
+    footer: footer,
+    line: line,
+    circle: circle,
+    text: text
 }
+
